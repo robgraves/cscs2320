416e7f1a4b4ea7f2c4eeb93957609829cc99b9a5
robgraves <matthewpage2003@yahoo.com>
2015-08-29 19:36:33 -0400

    started writing arraylist.c for dsi0 project in data structures.

---
+++ arraylist.c
@@ -0,0 +1,39 @@
+/**************************************************
+ *
+ *  Matthew Page
+ *  08/29/2015
+ *  CSCS2320 - Data Structures
+ *  Fall 2015
+ *
+ *  arraylist.c -   project dsi0 for data structs
+ *                  where creating a program that
+ *                  presents the user with a menu  
+ *                  to build list, display list, 
+ *                  insert into list, append into
+ *                  list, obtain from list, clear
+ *                  list, and quit.  This first 
+ *                  project will be using an array
+ *                  and functions for each option
+ *                  in the menu, in later projects
+ *                  we will use linked lists with 
+ *                  nodes.
+ *
+ *************************************************/
+
+#include <stdio.h>
+
+main()
+{
+    printf("Data Structures Array List Program Menu\n");
+    printf("---------------------------------------\n");
+    printf("Please choose one of the following:\n");
+    printf("1 - Build List\n");
+    printf("2 - Display List\n");
+    printf("3 - Insert into List\n");
+    printf("4 - Append into List\n");
+    printf("5 - Obtain from List\n");
+    printf("7 - Clear List\n");
+    printf("8 - Quit\n");
+    
+    return (0);
+}
