6516caf5f72726f53e49d05f6fb8eecac5041cdb
Matthew Page <me@matthewjpage.com>
2015-12-14 11:09:11 -0500

    started working on 0x3 sub-project grade script for eoce0

--- eoce0/app/list/0x3.c
+++ eoce0/app/list/0x3.c
@@ -3,6 +3,27 @@
 
 int main()
 {
-	// your implementation here
+    FILE *file  = NULL;
+    //tmp *node   = NULL;
+    //list *list  = NULL;
+    int num     = 0;
+
+    if ((file = fopen("status.flat", "r"))!=NULL)
+    {
+        while (num != -2)
+        {
+            fscanf(file, "%d", &num);
+            printf("%d", num);
+        }
+        fclose(file);
+        return 1;
+    }
+    else
+    {
+        //system("flattendata");
+        printf("File: status.flat does not exist!\n");
+        printf("Please run \"flattendata\" first.\n");
+    } 
+
 	return(0);
 }
